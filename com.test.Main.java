574174be73493298849b09e0090e07429b1d5617
wangnana863-dot <wangnana863@gmail.com>
2025-08-11 15:24:23 +0800

    Create com.test.Main.java

---
+++ com.test.Main.java
@@ -0,0 +1,8 @@
+public class Teacher {
+    private Long teacherId; // 编号
+    private String name; // 名字
+    private String subject; // 所教学科
+    private String phone; // 手机号
+
+    // 构造函数、Getter/Setter、toString 方法...
+}
