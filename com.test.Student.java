d349335bb6548f13def719d64bbf5f5b6d9d29c8
wangnana863-dot <wangnana863@gmail.com>
2025-08-11 15:26:41 +0800

    Create com.test.Student.java

---
+++ com.test.Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    private Long studentId; // 编号
+    private String name; // 名字
+    private Integer age; // 年龄
+    private String studentNumber; // 学号
+
+    // 构造函数、Getter/Setter、toString 方法...
+}
